10c17c68279d44475d6771a7ed6c9c4e1e89bf51
nobu72 <nobu7238@gmail.com>
2020-04-05 18:37:57 +0900

    会員登録実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user
+  def index
+  end
+
+  def new
+  end
+
+  def creare
+  end
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to new_user_session_path
+    end
+end
+
+
+def logged_in?
+  !current_user.nil?
+end
+
 end
