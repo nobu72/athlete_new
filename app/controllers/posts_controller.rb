dff022bf862dbb4407d4157467eadd3f4e281918
nobu72 <nobu7238@gmail.com>
2020-04-07 23:44:44 +0900

    post.indexに投稿を表示する
    次は最新１件の動画をindex に表示したい。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,26 @@
 class PostsController < ApplicationController
   before_action :logged_in_user
   def index
+    @posts = Post.all
+    @post = Post.all.order(updated_at: :desc).limit(1)
+
+    
   end
 
   def new
+    @post = Post.new
   end
 
-  def creare
-  end
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+    end
 
   private
+
+  def post_params
+    params.require(:post).permit(:text, :video)
+  end
 
   def logged_in_user
     unless logged_in?
